18a00576f3e83995ab1f2f0f80b9b75e9e1fdc1b
glauber <glauberabc@gmail.com>
2019-10-21 17:08:46 -0300

    Correçao no controle e alteraçao da senha

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'ee97810add33aa35dccacec4b7741f39bd2113fa',
   database: 'gobarber',
   define: {
     timestamp: true,
