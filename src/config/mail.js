3dc62b9aa6bc349ae4df730a9d440a5bd52a4ac5
glauber <glauberabc@gmail.com>
2019-10-22 15:42:36 -0300

    Implementaçoes aula 3 - Parte 2

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b53785edadc441',
+    pass: '8269945290fb62',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
